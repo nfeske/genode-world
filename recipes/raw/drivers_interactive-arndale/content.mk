87cf043598029699d6f2873e6bd59a7514411f7d
Norman Feske <norman.feske@genode-labs.com>
2020-07-17 15:37:27 +0200

    Replace input filter with event filter
    Issue genodelabs/genode#3827

--- recipes/raw/drivers_interactive-arndale/content.mk
+++ recipes/raw/drivers_interactive-arndale/content.mk
@@ -1,10 +1,10 @@
-content: drivers.config input_filter.config en_us.chargen special.chargen
+content: drivers.config event_filter.config en_us.chargen special.chargen
 
 drivers.config:
 	cp $(REP_DIR)/recipes/raw/drivers_interactive-arndale/$@ $@
 
-input_filter.config:
+event_filter.config:
 	cp $(REP_DIR)/recipes/raw/drivers_interactive-panda/$@ $@
 
 en_us.chargen special.chargen:
-	cp $(GENODE_DIR)/repos/os/src/server/input_filter/$@ $@
+	cp $(GENODE_DIR)/repos/os/src/server/event_filter/$@ $@
--- recipes/raw/drivers_interactive-panda/content.mk
+++ recipes/raw/drivers_interactive-panda/content.mk
@@ -1,7 +1,7 @@
-content: drivers.config input_filter.config en_us.chargen special.chargen
+content: drivers.config event_filter.config en_us.chargen special.chargen
 
-drivers.config input_filter.config:
+drivers.config event_filter.config:
 	cp $(REP_DIR)/recipes/raw/drivers_interactive-panda/$@ $@
 
 en_us.chargen special.chargen:
-	cp $(GENODE_DIR)/repos/os/src/server/input_filter/$@ $@
+	cp $(GENODE_DIR)/repos/os/src/server/event_filter/$@ $@
