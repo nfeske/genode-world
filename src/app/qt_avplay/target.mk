9c5df722ea3ba765aba10a6db11502f54efcf256
Norman Feske <norman.feske@genode-labs.com>
2020-08-11 21:46:18 +0200

    Update depot recipes for qt5_avplay
    Issue #222
    Issue genodelabs/genode#3795

--- src/app/qt_avplay/target.mk
+++ src/app/qt_avplay/target.mk
@@ -1,9 +1,9 @@
-include $(call select_from_repositories,src/app/qt5/tmpl/target_defaults.inc)
+QMAKE_PROJECT_FILE = $(REP_DIR)/src/app/qt_avplay/qt_avplay.pro
 
-include $(call select_from_repositories,src/app/qt5/tmpl/target_final.inc)
+QMAKE_TARGET_BINARIES = qt_avplay
 
-LIBS += qt5_qgenodeviewwidget qoost base
+QT5_PORT_LIBS = libQt5Core libQt5Gui libQt5PrintSupport libQt5Widgets
 
-REP_INC_DIR += include/qt5/qpa_genode
+LIBS = libc libm mesa qt5_component stdcxx qoost $(QT5_PORT_LIBS)
 
-CC_CXX_WARN_STRICT =
+include $(call select_from_repositories,lib/import/import-qt5_qmake.mk)
